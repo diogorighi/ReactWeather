8479e68d7e34491efcf595a6afb75aecafda516f
Diogo Righi <diogorighi@gmail.com>
2016-07-24 14:37:35 -0300

    Changes on about component

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -2,7 +2,8 @@ var React = require('react');
 
 var About = (props) => {
     return (
-        <h2>About Component</h2>
+        <h2>About</h2>
+        <p>This is the about page!</p>
     );
 };
 
